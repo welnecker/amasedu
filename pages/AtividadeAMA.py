8c75daaa6889becd7cf51033f971ac18cdc62b38
Janio Donisete Welnecker <jdwelnecker@sedu.es.gov.br>
2025-03-31 08:39:01 -0300

    1

--- pages/AtividadeAMA.py
+++ pages/AtividadeAMA.py
@@ -108,7 +108,7 @@ else:
 
                 for idx in st.session_state.atividades_exibidas:
                     nome_atividade = dados.loc[idx, "ATIVIDADE"]
-                    url_img = f"https://questoesama.pages.dev/{nome_atividade}.jpg"
+                    url_img = f"https://questoesama.pages.dev/img/{nome_atividade}.jpg"
 
                     try:
                         # 🔇 PROXY DESATIVADO — descomente se necessário futuramente
