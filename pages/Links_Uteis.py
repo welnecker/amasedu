1380fcb63a358ded28a794ded781c5f4ad962346
Janio Donisete Welnecker <jdwelnecker@sedu.es.gov.br>
2025-04-08 14:17:22 -0300

    1

---
+++ pages/Links_Uteis.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Links Úteis", page_icon="🔗")
+st.title("🔗 Links Úteis")
+
+st.markdown("### 📚 Currículo de Nivelamento e AMA")
+st.markdown("[Acesse aqui](https://curriculo.sedu.es.gov.br/curriculo/nivelamentoeama/)")
