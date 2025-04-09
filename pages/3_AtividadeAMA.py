c9e9b47f0487726061c2b28b6c1186a9d822dab4
Janio Donisete Welnecker <jdwelnecker@sedu.es.gov.br>
2025-04-09 10:06:06 -0300

    1

--- pages/3_AtividadeAMA.py
+++ pages/3_AtividadeAMA.py
@@ -146,7 +146,10 @@ if "codigo_atividade" in st.session_state and "pdf_bytes" in st.session_state:
     )
 
 with col_cancelar:
-    if st.button("❌ CANCELAR E RECOMEÇAR"):
+    if st.button("❌ CANCELAR E RECOMEÇAR") and "reiniciando" not in st.session_state:
+        st.toast("🔁 Reiniciando a aplicação...")
+        st.session_state["reiniciando"] = True  # trava múltiplos cliques
         st.session_state.clear()
         st.switch_page("pages/1_Acesso_Professores")
+
 
